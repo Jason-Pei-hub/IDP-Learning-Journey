7343312d727fd83a332116273271bf29ed88dc4a
jason_pei <2570686112@qq.com>
2024-03-16 20:51:49 +0800

    失败了，无法完成写入的操作，在进行写入操作时会发生图像崩坏，还需要多多学习解决这个问题

--- 图像的读入/code/code/test_read.c
+++ 图像的读入/code/code/test_read.c
@@ -3,19 +3,34 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void F1(uint8_t* pImgRGB, int width, int height)
+//void F1(uint8_t* pImgRGB, int width, int height)
+//{
+//    uint8_t* pCur;
+//    uint8_t* pEnd;
+//
+//    pEnd = pImgRGB + width * height * 3;  // 每个像素点占用 3 个字节（BGR）
+//    for (pCur = pImgRGB; pCur < pEnd; pCur += 3) {
+//        uint8_t blue = pCur[0];
+//        uint8_t green = pCur[1];
+//        uint8_t red = pCur[2];
+//        printf("R: %d, G: %d, B: %d ", red, green, blue);
+//    }
+//}
+
+void F1(uint8_t* pImg, int width, int height)
 {
     uint8_t* pCur;
     uint8_t* pEnd;
 
-    pEnd = pImgRGB + width * height * 3;  // 每个像素点占用 3 个字节（BGR）
-    for (pCur = pImgRGB; pCur < pEnd; pCur += 3) {
-        uint8_t blue = pCur[0];
-        uint8_t green = pCur[1];
-        uint8_t red = pCur[2];
-        printf("R: %d, G: %d, B: %d ", red, green, blue);
+    pEnd = pImg + width * height;
+    for (pCur = pImg; pCur < pEnd; pCur++)
+    {
+        //*pCur = f(*pCur);
+        printf("%d ", *pCur);  // 输出灰度值
     }
+    return;
 }
+
 
 int main()
 {
@@ -29,11 +44,11 @@ int main()
     int width = *(int*)&bmpHeader[18];   // 宽度信息位于偏移量为 18 的位置
     int height = *(int*)&bmpHeader[22];  // 高度信息位于偏移量为 22 的位置
 
-    uint8_t* imageData = (uint8_t*)malloc(width * height * 3);  // 每个像素点占用 3 个字节（BGR）
+    uint8_t* imageData = (uint8_t*)malloc(width * height);  // 每个像素点占用 3 个字节（BGR）
 
     fseek(file, 54, SEEK_SET);  // 跳过 BMP 文件头
 
-    fread(imageData, 3, width * height, file);  // 每个像素点占用 3 个字节（BGR）
+    fread(imageData, 1, width * height, file);  // 每个像素点占用 3 个字节（BGR）
 
     F1(imageData, width, height);
 
